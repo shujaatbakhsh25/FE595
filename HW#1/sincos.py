53359723d53a4b1650e23b0386ce21d13bd4b840
gvraman <gaurav.venkatraman@gmail.com>
2019-09-10 18:28:47 -0400

    Tan function implemented

--- HW#1/sincos.py
+++ HW#1/sincos.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.arange(-4 * np.pi, 4 * np.pi, 0.01)
+x = np.arange(0, 2 * np.pi, 0.1)
 
 y = np.sin(x)
 z = np.cos(x)
+w = np.tan(x)
 
-plt.plot(x, y, x, z)
+plt.plot(x, y, x, z, x, w)
 plt.show()
